14bc3a3cf85b931a841163315bf75ca31dd3529d
Jeremy Stretch <jeremy@netverity.dev>
2021-03-08 13:36:56 -0500

    Closes #5938: Deprecate support for Python 3.6

--- docs/release-notes/version-2.11.md
+++ docs/release-notes/version-2.11.md
@@ -4,6 +4,9 @@
 
 **WARNING:** This is a beta release and is not suitable for production use. It is intended for development and evaluation purposes only. No upgrade path to the final v2.11 release will be provided from this beta, and users should assume that all data entered into the application will be lost.
 
+**Note:** NetBox v2.11 is the last major release that will support Python 3.6. Beginning with NetBox v2.12, Python 3.7 or
+later will be required.
+
 ### New Features
 
 #### Parent Interface Assignments ([#1519](https://github.com/netbox-community/netbox/issues/1519))
--- netbox/netbox/settings.py
+++ netbox/netbox/settings.py
@@ -16,7 +16,7 @@ from django.core.validators import URLValidator
 # Environment setup
 #
 
-VERSION = '2.10.6-dev'
+VERSION = '2.11-beta1'
 
 # Hostname
 HOSTNAME = platform.node()
@@ -28,6 +28,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if platform.python_version_tuple() < ('3', '6'):
     raise RuntimeError(
         "NetBox requires Python 3.6 or higher (current: Python {})".format(platform.python_version())
+    )
+# TODO: Remove in NetBox v2.12
+if platform.python_version_tuple() < ('3', '7'):
+    warnings.warn(
+        "Support for Python 3.6 will be dropped in NetBox v2.12. Please upgrade to Python 3.7 or later at your "
+        "earliest convenience."
     )
 
 
