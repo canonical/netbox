587335e3ba470905a1b890341a67fb4d5aa9c518
checktheroads <matt@allroads.io>
2021-05-26 09:07:45 -0700

    #6372: Fix legacy BS5 classes

--- netbox/extras/tables.py
+++ netbox/extras/tables.py
@@ -8,10 +8,10 @@ from .models import ConfigContext, JournalEntry, ObjectChange, Tag, TaggedItem
 
 CONFIGCONTEXT_ACTIONS = """
 {% if perms.extras.change_configcontext %}
-    <a href="{% url 'extras:configcontext_edit' pk=record.pk %}" class="btn btn-xs btn-warning"><i class="mdi mdi-pencil" aria-hidden="true"></i></a>
+    <a href="{% url 'extras:configcontext_edit' pk=record.pk %}" class="btn btn-sm btn-warning"><i class="mdi mdi-pencil" aria-hidden="true"></i></a>
 {% endif %}
 {% if perms.extras.delete_configcontext %}
-    <a href="{% url 'extras:configcontext_delete' pk=record.pk %}" class="btn btn-xs btn-danger"><i class="mdi mdi-trash-can-outline" aria-hidden="true"></i></a>
+    <a href="{% url 'extras:configcontext_delete' pk=record.pk %}" class="btn btn-sm btn-danger"><i class="mdi mdi-trash-can-outline" aria-hidden="true"></i></a>
 {% endif %}
 """
 
