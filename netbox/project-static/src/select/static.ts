e3008babfe6fef10555a06d3a12b494be654fd1b
checktheroads <matt@allroads.io>
2021-07-17 21:51:47 -0700

    #6372: Fix static select class regression from 08f0a5d

--- netbox/project-static/src/select/static.ts
+++ netbox/project-static/src/select/static.ts
@@ -2,7 +2,7 @@ import SlimSelect from 'slim-select';
 import { getElements } from '../util';
 
 export function initStaticSelect() {
-  for (const select of getElements<HTMLSelectElement>('.netbox-select-static')) {
+  for (const select of getElements<HTMLSelectElement>('.netbox-static-select')) {
     if (select !== null) {
       const label = document.querySelector(`label[for=${select.id}]`) as HTMLLabelElement;
 
