f43ec7c05d12b491d6747056ce919ce78876c659
jeremystretch <jstretch@ns1.com>
2021-12-15 11:03:38 -0500

    Add "add IP range" button to prefix IP ranges view

--- netbox/ipam/views.py
+++ netbox/ipam/views.py
@@ -488,6 +488,7 @@ class PrefixIPRangesView(generic.ObjectChildrenView):
         return {
             'bulk_querystring': f"vrf_id={instance.vrf.pk if instance.vrf else '0'}&parent={instance.prefix}",
             'active_tab': 'ip-ranges',
+            'first_available_ip': instance.get_first_available_ip(),
         }
 
 
