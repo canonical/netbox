4ebcdd2b8f740acb74d6de28793986928f216cdf
Arthur Hanson <worldnomad@gmail.com>
2022-11-03 06:29:45 -0700

    8072 move js code from template to static file (#10824)

--- netbox/netbox/settings.py
+++ netbox/netbox/settings.py
@@ -407,6 +407,7 @@ STATIC_URL = f'/{BASE_PATH}static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'project-static', 'dist'),
     os.path.join(BASE_DIR, 'project-static', 'img'),
+    os.path.join(BASE_DIR, 'project-static', 'js'),
     ('docs', os.path.join(BASE_DIR, 'project-static', 'docs')),  # Prefix with /docs
 )
 
