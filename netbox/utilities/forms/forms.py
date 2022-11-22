f56e3eb784a2c421f871c1486a14e53ad448a3a0
jeremystretch <jstretch@ns1.com>
2022-11-22 12:02:21 -0500

    Fixes #8058: Display server-side form errors inline with fields

--- netbox/utilities/forms/forms.py
+++ netbox/utilities/forms/forms.py
@@ -48,10 +48,16 @@ class BootstrapMixin:
         ]
 
         for field_name, field in self.fields.items():
+            css = field.widget.attrs.get('class', '')
 
             if field.widget.__class__ not in exempt_widgets:
-                css = field.widget.attrs.get('class', '')
-                field.widget.attrs['class'] = ' '.join([css, 'form-control']).strip()
+                field.widget.attrs['class'] = f'{css} form-control'
+
+            elif isinstance(field.widget, forms.CheckboxInput):
+                field.widget.attrs['class'] = f'{css} form-check-input'
+
+            elif isinstance(field.widget, forms.Select):
+                field.widget.attrs['class'] = f'{css} form-select'
 
             if field.required and not isinstance(field.widget, forms.FileInput):
                 field.widget.attrs['required'] = 'required'
@@ -59,13 +65,18 @@ class BootstrapMixin:
             if 'placeholder' not in field.widget.attrs and field.label is not None:
                 field.widget.attrs['placeholder'] = field.label
 
-            if field.widget.__class__ == forms.CheckboxInput:
-                css = field.widget.attrs.get('class', '')
-                field.widget.attrs['class'] = ' '.join((css, 'form-check-input')).strip()
+    def is_valid(self):
+        is_valid = super().is_valid()
 
-            if field.widget.__class__ == forms.Select:
-                css = field.widget.attrs.get('class', '')
-                field.widget.attrs['class'] = ' '.join((css, 'form-select')).strip()
+        # Apply is-invalid CSS class to fields with errors
+        if not is_valid:
+            for field_name in self.errors:
+                # Ignore e.g. __all__
+                if field := self.fields.get(field_name):
+                    css = field.widget.attrs.get('class', '')
+                    field.widget.attrs['class'] = f'{css} is-invalid'
+
+        return is_valid
 
 
 #
