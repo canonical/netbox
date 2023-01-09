13549474344f1845a17f0c1186b990fed556771a
kkthxbye <unknown>
2023-01-09 07:45:12 +0100

    Get the queue from QUEUE_MAPPINGS when deleting JobResults

--- netbox/extras/models/models.py
+++ netbox/extras/models/models.py
@@ -634,7 +634,8 @@ class JobResult(models.Model):
     def delete(self, *args, **kwargs):
         super().delete(*args, **kwargs)
 
-        queue = django_rq.get_queue("default")
+        rq_queue_name = get_config().QUEUE_MAPPINGS.get(self.obj_type.name, RQ_QUEUE_DEFAULT)
+        queue = django_rq.get_queue(rq_queue_name)
         job = queue.fetch_job(str(self.job_id))
 
         if job:
