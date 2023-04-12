8de252e34e428f331732de93243839993cfe48f7
Arthur Hanson <worldnomad@gmail.com>
2023-04-12 07:25:54 -0700

    11432 device field (#11567)
    * 11432 make device field on interface read-only on api edit call
    
    * 11432 make device field on interface read-only on api edit call
    
    * 11432 extend serializer change to mixin
    
    * 11432 add readonlydevicemixin to template serializers
    
    * 11432 change subclass ordering
    
    * 11432 fix device_type for template serializers
    
    * 11432 DRY
    
    * 11432 DRY
    
    * 11432 make internal var
    
    * 11432 change to model-level validation
    
    * 11432 fix fk accessor
    
    * Clean up validation error messages
    
    ---------
    
    Co-authored-by: jeremystretch <jstretch@netboxlabs.com>

--- netbox/dcim/models/device_component_templates.py
+++ netbox/dcim/models/device_component_templates.py
@@ -120,6 +120,12 @@ class ModularComponentTemplateModel(ComponentTemplateModel):
             ),
         )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Cache the original DeviceType ID for reference under clean()
+        self._original_device_type = self.device_type_id
+
     def to_objectchange(self, action):
         objectchange = super().to_objectchange(action)
         if self.device_type is not None:
@@ -130,6 +136,11 @@ class ModularComponentTemplateModel(ComponentTemplateModel):
 
     def clean(self):
         super().clean()
+
+        if self.pk is not None and self._original_device_type != self.device_type_id:
+            raise ValidationError({
+                "device_type": "Component templates cannot be moved to a different device type."
+            })
 
         # A component template must belong to a DeviceType *or* to a ModuleType
         if self.device_type and self.module_type:
