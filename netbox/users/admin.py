e64289e791329bb2157e607b0e646b95cd483cbc
Jeremy Stretch <jstretch@netboxlabs.com>
2023-08-01 14:35:28 -0400

    #12589: Remove obsolete admin resources

--- netbox/users/admin.py
+++ netbox/users/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
 
-#
-# Users & groups
-#
-
-# Unregister the built-in GroupAdmin and UserAdmin classes so that we can use our custom admin classes below
+# Unregister Django's built-in Group and User admin views
 admin.site.unregister(Group)
 admin.site.unregister(User)
