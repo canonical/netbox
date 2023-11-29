6dddb6c9d22f751dc50b4b0267a768236bd275b1
Jeremy Stretch <jstretch@netboxlabs.com>
2023-11-29 16:37:08 -0500

    Fixes #14199: Fix jobs count for reports with a custom name

--- netbox/extras/views.py
+++ netbox/extras/views.py
@@ -1073,7 +1073,7 @@ class ReportJobsView(ContentTypePermissionRequiredMixin, View):
         jobs = Job.objects.filter(
             object_type=object_type,
             object_id=module.pk,
-            name=report.name
+            name=report.class_name
         )
 
         jobs_table = JobTable(
