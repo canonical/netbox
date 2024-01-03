a5fa30e85163be2ed3837cbb4081dafe6b4dfa6a
Jeremy Stretch <jstretch@netboxlabs.com>
2024-01-03 11:15:37 -0500

    Closes #14092: Remove backward compatibility for importing extras.plugins

--- netbox/extras/plugins/utils.py
+++
@@ -1,7 +0,0 @@
-import warnings
-
-from netbox.plugins.utils import *
-
-
-# TODO: Remove in v4.0
-warnings.warn(f"{__name__} is deprecated. Import from netbox.plugins instead.", DeprecationWarning)
