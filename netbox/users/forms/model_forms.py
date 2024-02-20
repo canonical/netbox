50e5bb9717baebda8bc2c1b2ce132a622f988b79
Abhimanyu Saharan <asaharan@onemindservices.com>
2024-02-20 23:37:13 +0530

    added validation error for script and report constraint #15174

--- netbox/users/forms/model_forms.py
+++ netbox/users/forms/model_forms.py
@@ -380,6 +380,12 @@ class ObjectPermissionForm(BootstrapMixin, forms.ModelForm):
                 constraints = [constraints]
             for ct in object_types:
                 model = ct.model_class()
+
+                if model._meta.model_name in ['script', 'report']:
+                    raise forms.ValidationError({
+                        'constraints': _('Constraints are not supported for this object type.')
+                    })
+
                 try:
                     tokens = {
                         CONSTRAINT_TOKEN_USER: 0,  # Replace token with a null user ID
