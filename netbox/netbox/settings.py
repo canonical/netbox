8678d1a577c86ad09c46d38c6a3ac459c6e476be
Abhimanyu Saharan <asaharan@onemindservices.com>
2024-02-20 23:45:56 +0530

    removed associate_by_email #14946

--- netbox/netbox/settings.py
+++ netbox/netbox/settings.py
@@ -571,7 +571,6 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
-    'social_core.pipeline.social_auth.associate_by_email',
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'netbox.authentication.user_default_groups_handler',
