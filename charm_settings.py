c6c22bcca172244f848234b18ecbdf8e5d82a4b4
Javier de la Puente <javier.delapuente@canonical.com>
2024-02-27 19:17:42 +0100

    WIP make it work first, then maybe clean

---
+++ charm_settings.py
@@ -0,0 +1,4 @@
+from netbox.original_settings import *
+
+# very ugly all this module
+MIDDLEWARE.insert(MIDDLEWARE.index('django.middleware.security.SecurityMiddleware') + 1, 'whitenoise.middleware.WhiteNoiseMiddleware')
