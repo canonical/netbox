c5d356c377ee5437d1874b091455b5ffdba25570
Javier de la Puente <javier.delapuente@canonical.com>
2024-02-27 18:41:40 +0100

    Generating static content

--- charm_configuration.py
+++ charm_configuration.py
@@ -36,7 +36,7 @@ DATABASE = {
 # configuration exists for each. Full connection details are required in both sections, and it is strongly recommended
 # to use two separate database IDs.
 
-redis_hostname = os.environ['DJANGO_REDIS_HOSTNAME']
+redis_hostname = os.environ.get('DJANGO_REDIS_HOSTNAME', 'localhost')
 redis_port = os.environ.get('DJANGO_REDIS_PORT', 6379)
 redis_username = os.environ.get('DJANGO_REDIS_USERNAME', '')
 redis_password = os.environ.get('DJANGO_REDIS_PASSWORD', '')
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Base URL path if accessing NetBox within a directory. For example, if installed at https://example.com/netbox/, set:
 # BASE_PATH = 'netbox/'
-BASE_PATH = os.environ.get('DJANGO_SECRET_KEY', '')
+BASE_PATH = os.environ["DJANGO_BASE_PATH"]
 INTERNAL_IPS = ('127.0.0.1', '::1')
 
 # Enable custom logging. Please see the Django documentation for detailed guidance on configuring custom logs:
