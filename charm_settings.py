1646a08becec03b3e6cffa3a2f9cf65e6e86f7c5
Javier de la Puente <javier.delapuente@canonical.com>
2024-02-29 12:13:07 +0100

    force rerun actions

--- charm_settings.py
+++ charm_settings.py
@@ -1,4 +1,4 @@
 from netbox.original_settings import *
 
-# very ugly all this module
+# very ugly all this module.
 MIDDLEWARE.insert(MIDDLEWARE.index('django.middleware.security.SecurityMiddleware') + 1, 'whitenoise.middleware.WhiteNoiseMiddleware')
