e0f0b51edd604c89560ae319f9fcb21b28b7fd15
Javier de la Puente <javier.delapuente@canonical.com>
2024-03-07 07:31:28 +0100

    Working saml example

--- charm/src/charm.py
+++ charm/src/charm.py
@@ -9,6 +9,7 @@ import typing
 
 import ops
 import xiilib.django
+from charms.saml_integrator.v0.saml import SamlDataAvailableEvent, SamlRequires
 
 logger = logging.getLogger(__name__)
 
@@ -16,14 +17,20 @@ logger = logging.getLogger(__name__)
 class DjangoCharm(xiilib.django.Charm):
     """Django Charm service."""
 
+    _SAML_RELATION_NAME = "saml"
+
     def __init__(self, *args: typing.Any) -> None:
         """Initialize the instance.
 
         Args:
             args: passthrough to CharmBase.
         """
-        # pylint: disable=useless-parent-delegation
         super().__init__(*args)
+        self.framework.observe(self.on.create_super_user_action, self._on_create_super_user_action)
+        self.saml = SamlRequires(self)
+        self.framework.observe(self.saml.on.saml_data_available, self._on_saml_data_available)
+        self.framework.observe(self._ingress.on.ready, self._on_ingress_ready)
+        self.framework.observe(self._ingress.on.revoked, self._on_ingress_revoked)
 
     def gen_env(self) -> dict[str, str]:
         """Return the environment variables for django scripts.
@@ -34,7 +41,74 @@ class DjangoCharm(xiilib.django.Charm):
         env = super().gen_env()
         if self._ingress.url:
             env["DJANGO_BASE_URL"] = self._ingress.url
+        env |= self.saml_env()
         return env
+
+    def _on_saml_data_available(self, _: SamlDataAvailableEvent) -> None:
+        """TODO. Needed to restart the workload."""
+        self.reconcile()
+
+    def _on_ingress_revoked(self, _) -> None:
+        """TODO. Needed to restart the workload."""
+        self.reconcile()
+
+    def _on_ingress_ready(self, _) -> None:
+        """TODO. Needed to restart the workload."""
+        self.reconcile()
+
+    def saml_env(self):
+        """TODO."""
+        saml_data = {}
+        if (relation := self.model.get_relation(self._SAML_RELATION_NAME)) is None:
+            return saml_data
+
+        relation_data = (
+            {key: value for key, value in relation.data[relation.app].items() if key != "data"}
+            if relation.app
+            else {}
+        )
+
+        # Fields following https://github.com/canonical/charm-relation-interfaces/tree/main/interfaces/saml/v0
+        entity_id = relation_data.get("entity_id", "")
+        metadata_url = relation_data.get("metadata_url", "")
+        single_sign_on_service_redirect_url = relation_data.get(
+            "single_sign_on_service_redirect_url", ""
+        )
+        x509certs = relation_data.get("x509certs", "")
+
+        saml_data.update(
+            {
+                "SAML_ENTITY_ID": entity_id,
+                "SAML_METADATA_URL": metadata_url,
+                "SAML_SINGLE_SIGN_ON_SERVICE_REDIRECT_URL": single_sign_on_service_redirect_url,
+                "SAML_X509CERTS": x509certs.split(",")[0],  # Review this
+            }
+        )
+        return saml_data
+
+    def _on_create_super_user_action(self, event: ops.ActionEvent):
+        """TODO. This should go to Django 12 factor and generate the password randomly."""
+        container = self.unit.get_container(self._CONTAINER_NAME)
+        if not container.can_connect():
+            event.fail("django-app container is not ready")
+        try:
+            action_environment = {
+                "DJANGO_SUPERUSER_PASSWORD": str(event.params["password"]),
+                "DJANGO_SUPERUSER_USERNAME": event.params["username"],
+                "DJANGO_SUPERUSER_EMAIL": event.params["email"],
+            }
+            environment = self.gen_env()
+            logger.info("ENV %s", (action_environment | environment))
+            output, _ = container.exec(
+                ["python3", "manage.py", "createsuperuser", "--noinput"],
+                environment=(action_environment | environment),
+                combine_stderr=True,
+                working_dir=str(self._BASE_DIR / "app"),
+                user="_daemon_",
+            ).wait_output()
+            event.set_results({"output": output})
+        except ops.pebble.ExecError as e:
+            event.fail(e.stdout)
 
 
 if __name__ == "__main__":
--- charm/tests/integration/test_charm.py
+++ charm/tests/integration/test_charm.py
@@ -35,3 +35,48 @@ async def test_netbox_health(
             timeout=20,
         )
         assert res.status_code == 200
+
+
+@pytest.mark.usefixtures("netbox_app")
+async def test_saml_netbox(
+    get_unit_ips: Callable[[str], Coroutine[None, None, List[str]]]
+) -> None:
+    """
+    arrange: TODO
+    act: TODO
+    assert: TODO
+    """
+    # TODO, USE THIS INSTEAD:
+    # https://github.com/canonical/discourse-k8s-operator/pull/199#discussion_r1514531055
+    # https://github.com/canonical/synapse-operator/blob/main/tests/integration/test_nginx.py#L44
+    saml_app = await model.deploy(
+        "saml-integrator",
+        channel="latest/edge",
+        config={
+            "entity_id": "https://login.staging.ubuntu.com",
+            "metadata_url": "https://login.staging.ubuntu.com/saml/metadata",
+        },
+    )
+    await model.wait_for_idle(raise_on_blocked=True, status="active")
+    model.relate("netbox:saml", "saml-integrator:saml")
+    await model.wait_for_idle(raise_on_blocked=True, status="active")
+    # juju integrate saml-integrator:saml   synapse:saml
+
+    unit_ips = await get_unit_ips("netbox")
+    for unit_ip in unit_ips:
+
+        url = f"http://{unit_ip}:8000"
+        res = requests.get(
+            url,
+            timeout=20,
+        )
+        assert res.status_code == 200
+        assert b"<title>Home | NetBox</title>" in res.content
+
+        # Also  some random thing from the static dir.
+        url = f"http://{unit_ip}:8000/static/netbox.ico"
+        res = requests.get(
+            url,
+            timeout=20,
+        )
+        assert res.status_code == 200
