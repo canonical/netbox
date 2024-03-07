5f56e2daffa1d4600a0a4e688d787d54103a340c
Arthur <worldnomad@gmail.com>
2024-03-07 09:58:26 -0800

    9856 fix wireless FK

--- netbox/wireless/graphql/types.py
+++ netbox/wireless/graphql/types.py
@@ -33,6 +33,9 @@ class WirelessLANGroupType(OrganizationalObjectType):
     filters=WirelessLANFilter
 )
 class WirelessLANType(NetBoxObjectType):
+    group: Annotated["WirelessLANGroupType", strawberry.lazy('wireless.graphql.types')] | None
+    vlan: Annotated["VLANType", strawberry.lazy('ipam.graphql.types')] | None
+    tenant: Annotated["TenantType", strawberry.lazy('tenancy.graphql.types')] | None
 
     @strawberry_django.field
     def interfaces(self) -> List[Annotated["InterfaceType", strawberry.lazy('dcim.graphql.types')]]:
@@ -45,4 +48,8 @@ class WirelessLANType(NetBoxObjectType):
     filters=WirelessLinkFilter
 )
 class WirelessLinkType(NetBoxObjectType):
-    pass
+    interface_a: Annotated["InterfaceType", strawberry.lazy('dcim.graphql.types')]
+    interface_b: Annotated["InterfaceType", strawberry.lazy('dcim.graphql.types')]
+    tenant: Annotated["TenantType", strawberry.lazy('tenancy.graphql.types')] | None
+    _interface_a_device: Annotated["DeviceType", strawberry.lazy('dcim.graphql.types')] | None
+    _interface_b_device: Annotated["DeviceType", strawberry.lazy('dcim.graphql.types')] | None
