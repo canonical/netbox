f960d5a4829df989f652b9f1b9b2381c60b4c79c
Arthur <worldnomad@gmail.com>
2024-03-11 08:33:32 -0700

    9856 fix plugin schema

--- netbox/netbox/tests/dummy_plugin/graphql.py
+++ netbox/netbox/tests/dummy_plugin/graphql.py
@@ -14,7 +14,6 @@ class DummyModelType:
     pass
 
 
-"""
 @strawberry.type
 class DummyQuery:
     @strawberry.field
@@ -22,18 +21,7 @@ class DummyQuery:
         return None
     dummymodel_list: List[DummyModelType] = strawberry_django.field()
 
-schema = strawberry.Schema(
-    query=DummyQuery,
-    # config=StrawberryConfig(auto_camel_case=False),
-)
-"""
 
-
-@strawberry.type
-class Query:
-    fruits: list[int] = strawberry_django.field()
-
-
-schema2 = strawberry.Schema(
-    query=Query,
-)
+schema = [
+    DummyQuery,
+]
