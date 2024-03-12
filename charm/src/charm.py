8edd652279e15cf5dd5b70dd57aef336c9f1116c
javierdelapuente <javier.delapuente@canonical.com>
2024-03-12 15:18:51 +0100

    Netbox SAML integration (#10)

--- charm/src/charm.py
+++ charm/src/charm.py
@@ -6,9 +6,11 @@
 
 import logging
 import typing
+import urllib.parse
 
 import ops
 import xiilib.django
+from charms.saml_integrator.v0.saml import SamlDataAvailableEvent, SamlRequires
 
 logger = logging.getLogger(__name__)
 
@@ -16,14 +18,19 @@ logger = logging.getLogger(__name__)
 class DjangoCharm(xiilib.django.Charm):
     """Django Charm service."""
 
+    _SAML_RELATION_NAME = "saml"
+
     def __init__(self, *args: typing.Any) -> None:
         """Initialize the instance.
 
         Args:
             args: passthrough to CharmBase.
         """
-        # pylint: disable=useless-parent-delegation
         super().__init__(*args)
+        self.saml = SamlRequires(self)
+        self.framework.observe(self.saml.on.saml_data_available, self._on_saml_data_available)
+        self.framework.observe(self._ingress.on.ready, self._on_ingress_ready)
+        self.framework.observe(self._ingress.on.revoked, self._on_ingress_revoked)
 
     def gen_env(self) -> dict[str, str]:
         """Return the environment variables for django scripts.
@@ -34,7 +41,68 @@ class DjangoCharm(xiilib.django.Charm):
         env = super().gen_env()
         if self._ingress.url:
             env["DJANGO_BASE_URL"] = self._ingress.url
+            # This may be problematic, as it could return http instead of https.
+            # In that case, the config option saml_sp_entity_id should be set.
+            if "DJANGO_SAML_SP_ENTITY_ID" not in env:
+                parsed_ingress_url = urllib.parse.urlparse(self._ingress.url)
+                parsed_ingress_url = parsed_ingress_url._replace(path="")
+                env["DJANGO_SAML_SP_ENTITY_ID"] = parsed_ingress_url.geturl()
+        env |= self.saml_env()
         return env
+
+    def saml_env(self) -> dict[str, str]:
+        """Environment variables for SAML.
+
+        Returns:
+           dict with environment variables.
+        """
+        if (relation := self.model.get_relation(self._SAML_RELATION_NAME)) is None:
+            return {}
+
+        if not relation.app:
+            return {}
+
+        relation_data = {
+            key: value for key, value in relation.data[relation.app].items() if key != "data"
+        }
+
+        # Fields defined in :
+        # https://github.com/canonical/charm-relation-interfaces/tree/main/interfaces/saml/v0
+        entity_id = relation_data.get("entity_id", "")
+        metadata_url = relation_data.get("metadata_url", "")
+        single_sign_on_service_redirect_url = relation_data.get(
+            "single_sign_on_service_redirect_url", ""
+        )
+        x509certs = relation_data.get("x509certs", "")
+
+        # saml_env should not raise. Pending to update this code once
+        # the next Django 12 factor PR is merged, and maybe set the
+        # unit to blocked if for any reason the saml integration data
+        # is wrong.
+        if x509certs:
+            x509cert = x509certs.split(",")[0]
+        else:
+            logger.error("Empty x509certs in saml")
+            x509cert = ""
+
+        return {
+            "SAML_ENTITY_ID": entity_id,
+            "SAML_METADATA_URL": metadata_url,
+            "SAML_SINGLE_SIGN_ON_SERVICE_REDIRECT_URL": single_sign_on_service_redirect_url,
+            "SAML_X509CERTS": x509cert,
+        }
+
+    def _on_saml_data_available(self, _: SamlDataAvailableEvent) -> None:
+        """Handle event for Saml data available."""
+        self.reconcile()
+
+    def _on_ingress_revoked(self, _: ops.HookEvent) -> None:
+        """Handle event for ingress revoked."""
+        self.reconcile()
+
+    def _on_ingress_ready(self, _: ops.HookEvent) -> None:
+        """Handle event for ingress ready."""
+        self.reconcile()
 
 
 if __name__ == "__main__":
--- charm/tests/integration/conftest.py
+++ charm/tests/integration/conftest.py
@@ -15,6 +15,7 @@ from juju.application import Application
 from juju.model import Model
 from pytest import Config
 from pytest_operator.plugin import OpsTest
+from saml_test_helper import SamlK8sTestHelper
 
 from tests.conftest import NETBOX_IMAGE_PARAM
 
@@ -57,10 +58,68 @@ async def get_unit_ips_fixture(
     return get_unit_ips
 
 
+@pytest.fixture(scope="module", name="saml_app_name")
+def saml_app_name_fixture() -> str:
+    """Return the name of the saml-integrator application deployed for tests."""
+    return "saml-integrator"
+
+
+@pytest.fixture(scope="module", name="nginx_app_name")
+def nginx_app_name_fixture() -> str:
+    """Return the name of the nginx-ingress-integrator application deployed for tests."""
+    return "nginx-ingress-integrator"
+
+
 @pytest.fixture(scope="module", name="postgresql_app_name")
 def postgresql_app_name_fixture() -> str:
     """Return the name of the postgresql application deployed for tests."""
     return "postgresql-k8s"
+
+
+@pytest.fixture(scope="module", name="netbox_app_name")
+def netbox_app_name_fixture() -> str:
+    """Return the name of the netbox application deployed for tests."""
+    return "netbox"
+
+
+@pytest.fixture(scope="module", name="netbox_hostname")
+def netbox_hostname_fixture() -> str:
+    """Return the name of the netbox hostname used for tests."""
+    return "netbox.internal"
+
+
+@pytest.fixture(scope="module", name="redis_app_name")
+def redis_app_name_fixture() -> str:
+    """Return the name of the redis application deployed for tests."""
+    return "redis-k8s"
+
+
+@pytest_asyncio.fixture(scope="module", name="nginx_app")
+async def nginx_app_fixture(
+    ops_test: OpsTest,
+    nginx_app_name: str,
+    model: Model,
+    pytestconfig: Config,
+) -> Application:
+    """Deploy nginx."""
+    async with ops_test.fast_forward():
+        app = await model.deploy(nginx_app_name, channel="latest/edge", revision=88, trust=True)
+        await model.wait_for_idle()
+    return app
+
+
+@pytest_asyncio.fixture(scope="module", name="saml_app")
+async def saml_app_fixture(
+    ops_test: OpsTest,
+    saml_app_name: str,
+    model: Model,
+    pytestconfig: Config,
+) -> Application:
+    """Deploy saml."""
+    async with ops_test.fast_forward():
+        app = await model.deploy(saml_app_name, channel="latest/edge")
+        await model.wait_for_idle()
+    return app
 
 
 @pytest_asyncio.fixture(scope="module", name="postgresql_app")
@@ -68,17 +127,18 @@ async def postgresql_app_fixture(
     ops_test: OpsTest,
     postgresql_app_name: str,
     model: Model,
+    pytestconfig: Config,
 ) -> Application:
     """Deploy postgresql."""
     async with ops_test.fast_forward():
         app = await model.deploy(postgresql_app_name, channel="14/stable", trust=True)
-        await model.wait_for_idle(status="active")
+        await model.wait_for_idle(apps=[postgresql_app_name], status="active")
     return app
 
 
 @pytest_asyncio.fixture(scope="module", name="netbox_app_image")
 def netbox_app_image_fixture(pytestconfig: Config) -> str:
-    """Get value from parameter django-app--image."""
+    """Get value from parameter netbox-image."""
     netbox_app_image = pytestconfig.getoption(NETBOX_IMAGE_PARAM)
     assert netbox_app_image, f"{NETBOX_IMAGE_PARAM} must be set"
     return netbox_app_image
@@ -101,16 +161,19 @@ async def netbox_app_fixture(
     model: Model,
     netbox_charm: str,
     netbox_app_image: str,
+    netbox_app_name: str,
     postgresql_app_name: str,
     get_unit_ips,
+    redis_app_name: str,
     redis_password: str,
     postgresql_app: Application,
+    pytestconfig: Config,
 ) -> Application:
     """Deploy netbox app."""
     resources = {
         "django-app-image": netbox_app_image,
     }
-    redis_ips = await get_unit_ips("redis-k8s")
+    redis_ips = await get_unit_ips(redis_app_name)
     app = await model.deploy(
         f"./{netbox_charm}",
         resources=resources,
@@ -123,18 +186,22 @@ async def netbox_app_fixture(
     )
     # If update_status comes before pebble ready, the unit gets to
     # error state. Just do not fail in that case.
-    await model.wait_for_idle(apps=["netbox"], status="waiting", raise_on_error=False)
+    await model.wait_for_idle(apps=[netbox_app_name], status="waiting", raise_on_error=False)
 
-    await model.relate("netbox:postgresql", f"{postgresql_app_name}")
-    await model.wait_for_idle(status="active")
+    await model.relate(f"{netbox_app_name}:postgresql", f"{postgresql_app_name}")
+    await model.wait_for_idle(apps=[netbox_app_name, postgresql_app_name], status="active")
     return app
 
 
 @pytest_asyncio.fixture(scope="module", name="redis_app")
-async def redis_app_fixture(model: Model) -> Application:
+async def redis_app_fixture(
+    redis_app_name: str,
+    model: Model,
+    pytestconfig: Config,
+) -> Application:
     """Deploy redis-k8s."""
-    app = await model.deploy("redis-k8s", channel="edge")
-    await model.wait_for_idle(apps=["redis-k8s"], status="active")
+    app = await model.deploy(redis_app_name, channel="edge")
+    await model.wait_for_idle(apps=[redis_app_name], status="active")
     return app
 
 
@@ -142,10 +209,11 @@ async def redis_app_fixture(model: Model) -> Application:
 async def redis_password_fixture(
     model: Model,
     redis_app: Application,
+    redis_app_name: str,
 ) -> str:
     """Get redis password from action."""
     password_action: Action = (
-        await model.applications["redis-k8s"]
+        await model.applications[redis_app_name]
         .units[0]
         .run_action(  # type: ignore
             "get-initial-admin-password",
@@ -154,3 +222,82 @@ async def redis_password_fixture(
     await password_action.wait()
     assert password_action.status == "completed"
     return password_action.results["redis-password"]
+
+
+@pytest_asyncio.fixture(scope="function", name="netbox_nginx_integration")
+async def netbox_nginx_integration_fixture(
+    model: Model,
+    nginx_app: Application,
+    netbox_app: Application,
+    netbox_hostname: str,
+):
+    """Integrate Netbox and Nginx for ingress integration."""
+    await nginx_app.set_config({"service-hostname": netbox_hostname, "path-routes": "/"})
+    await model.wait_for_idle()
+    relation = await model.add_relation(f"{netbox_app.name}", f"{nginx_app.name}")
+    await model.wait_for_idle(
+        apps=[netbox_app.name, nginx_app.name], idle_period=30, status="active"
+    )
+    yield relation
+    await netbox_app.destroy_relation("ingress", f"{nginx_app.name}:ingress")
+
+
+@pytest_asyncio.fixture(scope="module", name="saml_helper")
+async def saml_helper_fixture(
+    model: Model,
+) -> SamlK8sTestHelper:
+    """Fixture for SamlHelper."""
+    saml_helper = SamlK8sTestHelper.deploy_saml_idp(model.name)
+    return saml_helper
+
+
+@pytest_asyncio.fixture(scope="function", name="netbox_saml_integration")
+async def netbox_saml_integration_fixture(
+    model: Model,
+    saml_app: Application,
+    netbox_app: Application,
+    netbox_hostname: str,
+    saml_helper: SamlK8sTestHelper,
+):
+    """Integrate Netbox and SAML for saml integration."""
+    await netbox_app.set_config(
+        {
+            "saml_sp_entity_id": f"https://{netbox_hostname}",
+            # The saml Name for FriendlyName "uid"
+            "saml_username": "urn:oid:0.9.2342.19200300.100.1.1",
+        }
+    )
+    saml_helper.prepare_pod(model.name, f"{saml_app.name}-0")
+    saml_helper.prepare_pod(model.name, f"{netbox_app.name}-0")
+    await saml_app.set_config(
+        {
+            "entity_id": f"https://{saml_helper.SAML_HOST}/metadata",
+            "metadata_url": f"https://{saml_helper.SAML_HOST}/metadata",
+        }
+    )
+    await model.wait_for_idle(idle_period=30)
+    relation = await model.add_relation(saml_app.name, netbox_app.name)
+    await model.wait_for_idle(
+        apps=[saml_app.name, netbox_app.name],
+        idle_period=30,
+        status="active",
+    )
+
+    # For the saml_helper, a SAML XML metadata for the service is needed.
+    # There are instructions to generate it in:
+    # https://python-social-auth.readthedocs.io/en/latest/backends/saml.html#basic-usage.
+    # This one is instead a minimalistic one that works for the test.
+    metadata_xml = """
+    <md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" cacheDuration="P10D"
+                         entityID="https://netbox.internal">
+      <md:SPSSODescriptor protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol"
+                          AuthnRequestsSigned="false" WantAssertionsSigned="true">
+        <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
+                                     Location="https://netbox.internal/oauth/complete/saml/"
+                                     index="1"/>
+      </md:SPSSODescriptor>
+    </md:EntityDescriptor>
+    """
+    saml_helper.register_service_provider(name=netbox_hostname, metadata=metadata_xml)
+    yield relation
+    await netbox_app.destroy_relation("saml", f"{saml_app.name}:saml")
