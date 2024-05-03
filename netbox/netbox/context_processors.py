41504425ac12ed3142d834ba7e64bb97b9dbc13c
Jeremy Stretch <jstretch@netboxlabs.com>
2024-05-03 10:03:03 -0400

    Closes #15942: Refactor settings_and_registry() context processor

--- netbox/netbox/context_processors.py
+++ netbox/netbox/context_processors.py
@@ -1,18 +1,50 @@
 from django.conf import settings as django_settings
 
 from netbox.config import get_config
-from netbox.registry import registry
+from netbox.registry import registry as registry_
+
+__all__ = (
+    'config',
+    'preferences',
+    'registry',
+    'settings',
+)
 
 
-def settings_and_registry(request):
+def config(request):
     """
-    Expose Django settings and NetBox registry stores in the template context. Example: {{ settings.DEBUG }}
+    Adds NetBox configuration parameters to the template context. Example: {{ config.BANNER_LOGIN }}
+    """
+    return {
+        'config': get_config(),
+    }
+
+
+def preferences(request):
+    """
+    Adds preferences for the current user (if authenticated) to the template context.
+    Example: {{ preferences|get_key:"pagination.placement" }}
     """
     user_preferences = request.user.config if request.user.is_authenticated else {}
     return {
-        'settings': django_settings,
-        'config': get_config(),
-        'registry': registry,
         'preferences': user_preferences,
         'htmx_navigation': user_preferences.get('ui.htmx_navigation', False) == 'true'
+    }
+
+
+def registry(request):
+    """
+    Adds NetBox registry items to the template context. Example: {{ registry.models.core }}
+    """
+    return {
+        'registry': registry_,
+    }
+
+
+def settings(request):
+    """
+    Adds Django settings to the template context. Example: {{ settings.DEBUG }}
+    """
+    return {
+        'settings': django_settings,
     }
