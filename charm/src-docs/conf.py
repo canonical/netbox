0828ecbeb740bfec69c5422a62aea93862317ef9
javierdelapuente <javier.delapuente@canonical.com>
2024-11-26 13:32:37 +0100

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- charm/src-docs/conf.py
+++ charm/src-docs/conf.py
@@ -1,3 +1,6 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
